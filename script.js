b82c119d8e9db32ee2211942eb59966d64a64f39
RenzoWille <renzo.wille@hva.nl>
2024-12-06 09:31:06 +0100

    fixes #2

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ document.getElementById("hamburger").addEventListener("click", function() {
     var menu = document.getElementById("mobile-menu");
     menu.classList.toggle("active"); // Toggle the 'active' class to show/hide the menu
 });
+
+document.getElementById("allfunctionstoggle").addEventListener("click", function(e) {
+    e.preventDefault()
+    console.log('test')
+    document.querySelector('#items-list').classList.toggle('functionsVisible')
+});
