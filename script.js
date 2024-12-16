9c3adb63004487aaf88b42cfb68c8efc541d3297
RenzoWille <renzo.wille@hva.nl>
2024-12-16 12:29:39 +0100

    fixes #4

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-document.getElementById("hamburger").addEventListener("click", function() {
-    var menu = document.getElementById("mobile-menu");
-    menu.classList.toggle("active"); // Toggle the 'active' class to show/hide the menu
+document.getElementById("hamburger").addEventListener("click", function() { //Aanspreken van het hamburger element en een click event toevoegen
+    var menu = document.getElementById("mobile-menu"); 
+    menu.classList.toggle("active"); // Toggle de 'active' class om het menu te laten zien en te verbergen
 });
 
-document.getElementById("allfunctionstoggle").addEventListener("click", function(e) {
-    e.preventDefault()
+document.getElementById("allfunctionstoggle").addEventListener("click", function(e) { //Aanspreken van de allfunctionstoggle en een click event toevoegen
+    e.preventDefault() //De default settings 'preventen' zodat de pagina niet ververst na een klik op de button
     console.log('test')
-    document.querySelector('#items-list').classList.toggle('functionsVisible')
+    document.querySelector('#items-list').classList.toggle('functionsVisible')  //De item list laten zien wanneer er een click event plaatsvindt
 });
 
